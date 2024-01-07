b651b18d1694442e5cbf9ad7ff44f550f159ea94
Johnk-Mj <johnkatembue4@gmail.com>
2024-01-07 17:05:23 +0200

    feat(add) : modal examples

---
+++ examples/vanilla-js-ts/dialog/main.ts
@@ -0,0 +1,139 @@
+import { createModal } from "@unifyui-js/dialog_test"
+import '@unocss/reset/tailwind.css'
+import 'virtual:uno.css'
+
+
+const modalEl = document.querySelector("[data-modal-test-1]")
+if (modalEl instanceof HTMLElement) {
+    createModal({
+        modalElement: modalEl,
+        options: {
+
+        }
+    })
+}
+const modalEl1 = document.querySelector("[data-modal-test-1b]")
+if (modalEl1 instanceof HTMLElement) {
+    createModal({
+        modalElement: modalEl1,
+        options: {
+
+        }
+    })
+}
+const modalEl3 = document.querySelector("[data-modal-test-3]")
+if (modalEl3 instanceof HTMLElement) {
+    createModal({
+        modalElement: modalEl3,
+        options: {
+            animateContent: {
+                enterAnimation: "slideIn .5s linear",
+                exitAnimation: "slideOut .2s linear"
+            },
+            onHide() {
+                console.log("Modal closed")
+            },
+            onShow: () => {
+                console.log("Modal Open")
+            }
+        }
+    })
+}
+
+const modalEl2 = document.querySelector("[data-modal-test-2]")
+if (modalEl2 instanceof HTMLElement) {
+    createModal({
+        modalElement: modalEl2,
+        options: {
+            onHide() {
+                console.log("Modal closed")
+            },
+            onShow: () => {
+                console.log("Modal Open")
+            }
+        }
+    })
+}
+
+
+const modalEl4 = document.querySelector("[data-modal-prevent]")
+const closeModal = document.querySelector("[data-custom-trigger]") as HTMLElement
+if (modalEl4 instanceof HTMLElement) {
+    const b = createModal({
+        modalElement: modalEl4,
+        triggerElement:closeModal,
+        options: {
+            preventCloseModal:true,
+            animateContent: {
+                enterAnimation: "slideIn .3s linear",
+            },
+            onHide() {
+                console.log("Modal closed")
+            },
+            onShow: () => {
+                console.log("Modal Open")
+            }
+        }
+    })
+}
+
+const modalWithStacked = document.querySelector("[data-modal-with-stacked]")
+const openWithStacked = document.querySelector("[data-trigger-with-stacked]") as HTMLElement
+if (modalWithStacked instanceof HTMLElement) {
+    const b = createModal({
+        modalElement: modalWithStacked,
+        triggerElement:openWithStacked,
+        options: {
+            animateContent: {
+                enterAnimation: "slideIn .3s linear",
+            },
+            onHide() {
+                console.log("Modal closed")
+            },
+            onShow: () => {
+                console.log("Modal Open")
+            }
+        }
+    })
+}
+const modalEl5 = document.querySelector("[data-modal-prevent-stack]")
+const closeModal1 = document.querySelector("[data-custom-trigger-stack]") as HTMLElement
+if (modalEl5 instanceof HTMLElement) {
+    const b = createModal({
+        modalElement: modalEl5,
+        triggerElement:closeModal1,
+        options: {
+            enableStackedModals:true,
+            animateContent: {
+                enterAnimation: "slideIn .3s linear",
+            },
+            onHide() {
+                console.log("Modal closed")
+            },
+            onShow: () => {
+                console.log("Modal Open")
+            }
+        }
+    })
+}
+
+const modalEl6 = document.querySelector("[data-modal-third]")
+const closeModal2 = document.querySelector("[data-trigger-third]") as HTMLElement
+if (modalEl6 instanceof HTMLElement) {
+    const b = createModal({
+        modalElement: modalEl6,
+        triggerElement:closeModal2,
+        options: {
+            enableStackedModals:true,
+            animateContent: {
+                enterAnimation: "slideIn .3s linear",
+            },
+            onHide() {
+                console.log("Modal closed")
+            },
+            onShow: () => {
+                console.log("Modal Open")
+            }
+        }
+    })
+}
